f085dabae76bb96244163785e18e3a1d71c2e911
robbytrujillo <robbyikusuma88@gmail.com>
2025-11-05 04:58:49 +0700

    Create Models, Controllers And Resources

---
+++ app/Http/Controllers/CourseKeyPointController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CourseKeyPoint;
+use Illuminate\Http\Request;
+
+class CourseKeyPointController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CourseKeyPoint $courseKeyPoint)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CourseKeyPoint $courseKeyPoint)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CourseKeyPoint $courseKeyPoint)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CourseKeyPoint $courseKeyPoint)
+    {
+        //
+    }
+}
