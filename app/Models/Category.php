44e9d8dd74dc9f1b16f2083e41b0b01e8fd32abd
robbytrujillo <robbyikusuma88@gmail.com>
2025-11-09 05:34:20 +0700

    Mass Assignment Preparation

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
     //
+    use HasFactory, SoftDeletes;
+
+    // cara pertama dalam mempersiapkan mass assignment
+    protected $fillable = [
+        'name',
+        'slug',
+        'icon',
+    ];
+
+    // cara kedua, tapi user dapat memasukkan apa saja yang dapat membahayakan sistem
+    protected $guarded = [
+        'id',
+    ];
 }
