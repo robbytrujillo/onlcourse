1cc8424241ff1c2192148ccc3c855db664eeafa8
robbytrujillo <robbyikusuma88@gmail.com>
2025-11-11 09:05:20 +0700

    Preparation of views pages cms admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Support\Carbon;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
