114e6631bb456d863e42fb62a6c64d00c371ec36
robbytrujillo <robbyikusuma88@gmail.com>
2025-11-13 05:41:33 +0700

    Edit And Delete Category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
 use App\Models\Category;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
@@ -74,14 +75,32 @@ class CategoryController extends Controller
     public function edit(Category $category)
     {
         //
+        return view('admin.categories.edit', compact('category'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Category $category)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
         //
+        DB::transaction(function () use ($request) {    
+
+            $validated = $request->validated();
+
+            if ($request->hasFile('icon')) {
+                    $iconPath = $request->file('icon')->store('icons', 'public');
+                    $validated['icon'] = $iconPath;
+                } else {
+                    $iconPath = 'images/icon-default.png';
+                } 
+
+                $validated['slug'] = Str::slug($validated['name']);
+
+                $category = Category::create($validated);
+        });
+
+        return redirect()->route('admin.categories.index');
     }
 
     /**
--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -1,41 +1,43 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('New Category') }}
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            {{ __('Update Category') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden p-10 shadow-sm sm:rounded-lg">
+            <div class="p-10 overflow-hidden bg-white shadow-sm sm:rounded-lg">
 
                 @if($errors->any())
                     @foreach($errors->all() as $error)
-                        <div class="py-3 w-full rounded-3xl bg-red-500 text-white">
+                        <div class="w-full py-3 text-white bg-red-500 rounded-3xl">
                             {{$error}}
                         </div>
                     @endforeach
                 @endif
                 
-                <form method="POST" action="#" enctype="multipart/form-data">
+                <form method="POST" action="{{ route('admin.categories.update', $category) }}" enctype="multipart/form-data">
                     @csrf
-
+                    @method('PUT')
+                    
                     <div>
                         <x-input-label for="name" :value="__('Name')" />
-                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                        <x-text-input value="{{ $category->name }}" id="name" class="block w-full mt-1" type="text" name="name" required autofocus autocomplete="name" />
                         <x-input-error :messages="$errors->get('name')" class="mt-2" />
                     </div>
 
                     <div class="mt-4">
                         <x-input-label for="icon" :value="__('icon')" />
-                        <x-text-input id="icon" class="block mt-1 w-full" type="file" name="icon" required autofocus autocomplete="icon" />
+                        <img src="{{ Storage::url($category->icon) }}" alt="" class="rounded-2xl object-cover w-[90px] h-[90px]">
+                        <x-text-input id="icon" class="block w-full mt-1" type="file" name="icon" required autofocus autocomplete="icon" />
                         <x-input-error :messages="$errors->get('icon')" class="mt-2" />
                     </div>
 
                     <div class="flex items-center justify-end mt-4">
             
-                        <button type="submit" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
-                            Add New Category
+                        <button type="submit" class="px-6 py-4 font-bold text-white bg-indigo-700 rounded-full">
+                            Update Category
                         </button>
                     </div>
                 </form>
