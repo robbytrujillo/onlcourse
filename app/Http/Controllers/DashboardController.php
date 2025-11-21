652a772d8372f85e403d25911ab3d3e9cb26def2
robbytrujillo <robbyikusuma88@gmail.com>
2025-11-21 22:47:50 +0700

    Customizing Dashboard Reports

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
+use App\Models\CourseStudent;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
@@ -11,6 +14,15 @@ class DashboardController extends Controller
         $user = Auth::user();
 
         $courseQuery = Course::query();
+
+        if ($user->hasRole('teacher')) {
+            $courseQuery->whereHas('teacher', function($query) use ($user) {
+                $query->where('user_id', $user->id);
+            });
+
+            $students = CourseStudent::whereIn('course_id', $courseQuery->select('id'))
+            ->disctinc('user_id')
+        }
     }
 
 
